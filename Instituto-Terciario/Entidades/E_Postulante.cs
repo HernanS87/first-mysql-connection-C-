e8d49582a670e600ef44a91bec7287907c290042
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-10-19 19:37:21 -0300

    Creé E_Postulante y Postulantes

---
+++ Instituto-Terciario/Entidades/E_Postulante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituto_Terciario.Entidades
+{
+    public class E_Postulante
+    {
+        public int NPostu { get; set; }
+        public string? NombreP { get; set; }
+        public string? ApellidoP { get; set; }
+        public string? TDocP { get; set; }
+        public int DocP { get; set; }
+    }
+}
